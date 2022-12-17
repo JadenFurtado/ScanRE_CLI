a729df0cdf7cf94f8c43c6233072f775b06fa991
jaden <furtadojaden@gmail.com>
2022-12-18 01:36:31 +0530

    Add sanity checks and remove async from functions

--- Scanner.py
+++ Scanner.py
@@ -6,13 +6,26 @@ import json
 # common scanner class for scanning repositories
 class CodeScanner:
     def __init__(self,repoLink,path,repoName,scanResultPath):
-        self.repoLink=repoLink
-        self.path=path
-        self.repoName = repoName
-        self.scanResultPath = scanResultPath
+        # Adding checks to variables
+        if repoLink is None:
+            print("repoLink cannot be none")
+        else:
+            self.repoLink=repoLink
+        if path is None:
+            print("scan path cannot be none")
+        else:
+            self.path=path
+        if repoName is None:
+            print("repo name cannot be none")
+        else:
+            self.repoName = repoName
+        if scanResultPath is None:
+            print("scan result path cannot be none")
+        else:
+            self.scanResultPath = scanResultPath
     
     # clone code from source repository
-    async def getCode(self):
+    def getCode(self):
         try:
             os.system('git clone '+self.repoLink+" "+self.path+"/"+self.repoName+"/")
             return "success"
@@ -21,9 +34,9 @@ class CodeScanner:
             return "failed"
 
     # scan code using semgrep
-    async def scanCode(self):
+    def scanCode(self):
         try:
-            os.system('docker run --rm -v "'+self.path+'/'+self.repoName+':/src" returntocorp/semgrep semgrep --config=auto --output='+self.repoName+'.json --json --verbose')
+            os.system('docker run --rm -v "'+self.path+'/'+self.repoName+':/src" returntocorp/semgrep semgrep --config=auto --output=output.json --json --verbose')
             os.system('mv '+self.path+'/'+self.repoName+'/output.json '+self.scanResultPath+'/'+self.repoName+'.json')
             return "success"
         except:
@@ -31,13 +44,10 @@ class CodeScanner:
             return "failed"
 
     # delete the code to conserve memory
-    async def cleanUp(self):
+    def cleanUp(self):
         try:
-            os.system('rm -rf '+self.path+'/'+self.repoName+'/*')
-            os.system('rm -rf '+self.path+'/'+self.repoName+'/.git')
-            os.system('rmdir '+self.path+'/'+self.repoName)
+            os.system('rm -rf '+self.path+'/'+self.repoName)
             print("success")
             return "success"
         except:
             return "failed"
-
--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import os
 from Scanner import CodeScanner
+from dotenv import dotenv_values
+from dotenv import load_dotenv
+load_dotenv(".env")
 
+art = """
+
+█▀▀ █▀▀ █▀▀█ █▀▀▄ ░█▀▀█ ░█▀▀▀ 
+▀▀█ █   █▄▄█ █  █ ░█▄▄▀ ░█▀▀▀ 
+▀▀▀ ▀▀▀ ▀  ▀ ▀  ▀ ░█ ░█ ░█▄▄▄
+
+"""
 if __name__=='__main__':
+    print(art)
     targets = open("targets.txt","r")
     for target in targets:
-        target = target.strip()
-        values = target.split("/")
-        repoLink = target
-        path = os.getenv("SCAN_PATH")
-        repoName = values[4]
-        scanResultPath = os.getenv("RESULT_PATH")
-        print(values)
-        scanner = CodeScanner(repoLink,path,repoName,scanResultPath)
-        scanner.getCode()
-        scanner.scanCode()
-        scanner.cleanUp()
+        if target is None or target == "/n":
+            print("Target was empty")
+            print("exiting")
+            break
+        else:
+            target = target.strip()
+            values = target.split("/")
+            repoLink = target
+            # adding sanity checks
+            path = os.getenv("SCAN_PATH")
+            if path is None:            
+                exit("scan path cannot be null")
+            repoName = values[4]
+            if repoName is None:
+                exit("repo name cannot be null")
+            scanResultPath = os.getenv("RESULT_PATH")
+            if scanResultPath is None:
+                exit("scan result path cannot be null")
+            print(values)
+            scanner = CodeScanner(repoLink,path,repoName,scanResultPath)
+            scanner.getCode()
+            scanner.scanCode()
+            scanner.cleanUp()
     targets.close()
